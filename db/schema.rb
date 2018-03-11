a4ed93fb7bb1196ba63fd61f523e15e93b923d50
Shelley Obery <sa.obery@gmail.com>
2018-03-11 12:36:30 +0100

    added price and dynamic img tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309180930) do
+ActiveRecord::Schema.define(version: 20180311111255) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180309180930) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "medium"
+    t.decimal "price"
   end
 
 end
