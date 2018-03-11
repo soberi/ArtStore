97f52fc8d02e7bc3e46003569b925c2bff5809a5
Shelley Obery <sa.obery@gmail.com>
2018-03-11 09:59:41 +0100

    4.2 database migrations, adding products via seed & html

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307173703) do
+ActiveRecord::Schema.define(version: 20180309180930) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180307173703) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "medium"
   end
 
 end
