5f54073aba210ddf66558cfea537f15a862a9ff6
Shelley Obery <sa.obery@gmail.com>
2018-03-16 15:09:37 +0100

    attempting to fix product display issue on heroku, posibble js error

--- app/assets/javascripts/nav_collapse.js
+++ app/assets/javascripts/nav_collapse.js
@@ -2,12 +2,3 @@
 // clicks outside of element
 
 
-$(document).ready(function () {
-    $(document).click(function (event) {
-        var clickover = $(event.target);
-        var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse in");
-        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
-            $("button.navbar-toggle").click();
-        }
-    });
-});
