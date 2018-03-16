51a830cb49a051f25c88748861ad45e5945ad94c
Shelley Obery <sa.obery@gmail.com>
2018-03-16 14:46:05 +0100

    added partials for nav and carousel, fixed brand (temp) logo link, updated order pages scss

---
+++ app/assets/javascripts/nav_collapse.js
@@ -0,0 +1,13 @@
+// code to allow mobile collapsed nav to close when user 
+// clicks outside of element
+
+
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse in");
+        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+            $("button.navbar-toggle").click();
+        }
+    });
+});
