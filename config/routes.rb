d6ecb50db436b83928554acc5f2468045b7e316b
Shelley Obery <sa.obery@gmail.com>
2018-04-04 19:33:31 +0200

    5.1 editing routes, implemented strange redirects. Back to normal in next commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   get 'simple_pages/contact'
 
-  root 'simple_pages#landing_page'
+  get 'simple_pages/landing_page'
+
+  root 'orders#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
