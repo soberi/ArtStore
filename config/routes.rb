5d1188f3396af413a5c5669fe0a99a0435575185
Shelley Obery <sa.obery@gmail.com>
2018-04-06 10:33:19 +0200

    5.1 changed routes back to normal, show page update

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
 
   get 'simple_pages/contact'
 
-  get 'simple_pages/landing_page'
-
-  root 'orders#index'
+  root 'simple_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
