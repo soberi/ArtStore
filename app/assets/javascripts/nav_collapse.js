75810fd6bf65c8571b0a78c042c2149d3d46b225
Shelley Obery <sa.obery@gmail.com>
2018-04-09 12:23:30 +0200

    commented out js for collapse navbar

--- app/assets/javascripts/nav_collapse.js
+++ app/assets/javascripts/nav_collapse.js
@@ -2,12 +2,12 @@
 // clicks outside of element
 
 // need to fix so that when user clicks inside of search field the menu will not close
-$(document).ready(function () {
-    $(document).click(function (event) {
-        var clickover = $(event.target);
-        var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse in");
-        if (_opened === true && !clickover.hasClass("navbar-toggle")) {
-            $("button.navbar-toggle").click();
-        }
-    });
-});
+// $(document).ready(function () {
+//     $(document).click(function (event) {
+//         var clickover = $(event.target);
+//         var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse in");
+//         if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+//             $("button.navbar-toggle").click();
+//         }
+//     });
+// });
