904d1ee5814e05a0898fdca6d6c631787d0b663c
Shelley Obery <sa.obery@gmail.com>
2018-04-11 16:33:04 +0200

    5.7 fixed conflict in product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
-	has_many :orders
+
+has_many :orders
+has_many :comments
+
 
 	def self.search(search_term)
 		if Rails.env.development?
@@ -7,6 +10,14 @@ class Product < ApplicationRecord
 		else
 			Product.where("name ilike ?", "%#{search_term}%")
 		end
+	end
+
+	def highest_rating_comment
+		comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+		comments.rating_asc.first
 	end
 
 end
