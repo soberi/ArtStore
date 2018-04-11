59a42eaad7fbd6d7b8a4051c72e2c9cbc2e4bfab
Shelley Obery <sa.obery@gmail.com>
2018-04-11 16:03:19 +0200

    5.7 added comment model & associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+has_many :orders
+has_many :comments
 
 	def self.search(search_term)
 		if Rails.env.development?
@@ -6,6 +8,14 @@ class Product < ApplicationRecord
 		else
 			Product.where("name ilike ?", "%#{search_term}%")
 		end
+	end
+
+	def highest_rating_comment
+		comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+		comments.rating_asc.first
 	end
 
 end
