2e7c3f8e4a9cab1ccb2b27f22a3c9bd4945a0ff5
Shelley Obery <sa.obery@gmail.com>
2018-05-14 11:25:40 +0200

    5.11 devise validate, permit user :first_name, :last_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
   	redirect_to main_app.root_url, alert: exception.message
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 	
 	has_many :orders
 	has_many :comments
+
+	validates :first_name, presence: true
+	validates :last_name, presence: true 
 end
