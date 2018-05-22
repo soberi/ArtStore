b28c23df18ff4f7fbec2abba77f50d3020af78e6
Shelley Obery <sa.obery@gmail.com>
2018-05-22 11:46:26 +0200

    6.3 added functional tests for user + landing_page

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ gem 'cancancan', '~> 2.0'
 
 # Gemfile for Rails 3+, Sinatra, and Merb
 gem 'will_paginate', '~> 3.1.1'
+
+gem 'rails-controller-testing'
---
+++ spec/controllers/simple_pages_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe SimplePagesController, type: :controller do
+	context 'GET #landing_page' do
+		it 'renders the landing page template' do
+			get :landing_page
+			expect(response).to be_ok
+			expect(response). to render_template('landing_page')
+		end
+	end
+end
