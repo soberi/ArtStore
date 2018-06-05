49d03e18da420d844c6fda30bc71ee1d504dbceb
Shelley Obery <sa.obery@gmail.com>
2018-06-05 21:22:44 +0200

    6.14 added ActiveJob for realtime comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,6 @@ class CommentsController < ApplicationController
 		@user = current_user
 		respond_to do |format|
 			if @comment.save
-				ProductChannel.broadcast_to @product.id, 
-					comment: CommentsController.render(partial: 'comments/comment', 
-					locals: {comment: @comment, current_user: current_user}),
-					average_rating: @product.average_rating
 				format.html { redirect_to @product, notice: "Thanks for leaving a comment." }
 				format.json { render :show, status: :created, location: @product }
 				format.js
