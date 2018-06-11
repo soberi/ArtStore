f4ddc68b339d2d90a3479da67672b586a32908ad
Shelley Obery <sa.obery@gmail.com>
2018-06-11 18:07:12 +0200

    6.15 add payment confirmation emaul

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,18 +8,29 @@ class UserMailer < ApplicationMailer
 				to: "sa.obery@gmail.com",
 				subject: "A new message from #{name}")
 	end
-
-	def welcome(user)
-		attachments.inline['mailer_img.jpg'] = File.read('app/assets/images/mailer_img.jpg')
-		@appname = "Soberi ArtStore"
-		mail(to: user.email,
-				subject: "Welcome to #{@appname}" )
-	end
-
+	
 	def thank_you
 		@name = params[:name]
 		@email = [:email]
 		@message = [:message]
 		UserMailer.contact_form(@email, @name, @message).deliver_now
+	end
+	
+	def welcome(user)
+		@email = [:email]
+		attachments.inline['mailer_img.jpg'] = File.read('app/assets/images/mailer_img.jpg')
+		@appname = "Soberi ArtStore"
+		mail(to: @email,
+				subject: "Welcome to #{@appname}" )
+	end
+
+	def order_confirmation(email, name, product, price)
+		attachments.inline['mailer_img.jpg'] = File.read('app/assets/images/mailer_img.jpg')
+		@appname = "Soberi ArtStore"
+		@name = name
+		@email = [:email]
+		@product = product
+		@price = price
+			mail(to: email, subject: "Your #{@appname} order")
 	end
 end
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -3,4 +3,13 @@ class UserMailerPreview < ActionMailer::Preview
 	def contact_form
 		UserMailer.contact_form("john@example.com", "John", "Hey there, sailor!")		
 	end
+
+	def welcome
+		UserMailer.welcome("john@example.com")
+	end
+
+	def order_confirmation
+		UserMailer.order_confirmation("john@example.com", "John", "Drache", "10.00€")
+	end
+
 end
